373b852cac675643295773b109fd3936a5e91622
JulianF <juliandavidbf@gmail.com>
2022-10-09 13:48:00 -0400

    @feat: create new components

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent implements OnInit {
+
+  @Input() img = 'valor inicial'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
