f372500f03cd1533b7106feb85926cd8c594c3f8
JulianF <juliandavidbf@gmail.com>
2022-10-14 10:37:06 -0400

    @feat: Create component image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = 'my store';
+  imgParent = 'https://picsum.photos/200';
 }
