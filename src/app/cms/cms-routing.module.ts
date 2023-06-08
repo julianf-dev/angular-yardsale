8379ebc37bf133bf835af3ab1121e304803e8d5b
Julian F <juliandavidbf@gmail.com>
2023-06-08 16:04:39 -0400

    Retomando projecto

--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { GridComponent } from './pages/grid/grid.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path:'grid',
+        component: GridComponent
+      },
+      {
+        path:'tasks',
+        component: TasksComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/grid',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
